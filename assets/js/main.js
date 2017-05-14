6300130f5c484402e91938ca0fe55585a75549e8
Gavin Barnett <gavbarnett@gmail.com>
2017-05-14 10:25:32 +0100

    FSK corrections
    Fixing Timeing on FSK tones

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,59 +13,59 @@ var frameCount = (audioCtx.sampleRate * oversamplerate); //48kHz
 var myArrayBuffer;
 
 function whitenoise() {
-    // Fill the buffer with white noise;
-    //just random values between -1.0 and 1.0
-    // This gives us the actual array that contains the data
-    var nowBuffering = [];
-    for (var i = 0; i < frameCount; i++) {
-        nowBuffering[i] = Math.random() * 2 - 1;
-    }
-    playbuffer(nowBuffering);
+  // Fill the buffer with white noise;
+  //just random values between -1.0 and 1.0
+  // This gives us the actual array that contains the data
+  var nowBuffering = [];
+  for (var i = 0; i < frameCount; i++) {
+    nowBuffering[i] = Math.random() * 2 - 1;
+  }
+  playbuffer(nowBuffering);
 }
 
 function FSK(Freq, Loops) {
-    console.clear();
-    // Fill the buffer with 9k6;
-    //just random values between -1.0 and 1.0
-    // This gives us the actual array that contains the data
-    var nowBuffering = [];
-    var t = 0;
-    var p = 1 / (audioCtx.sampleRate * oversamplerate);
-    var sel = 0;
-    var data = "Data:- ";
-    var f = Freq[sel];
-    for (var i = 0; i < frameCount; i++) {
-        if (t >= (Loops / f)) {
-            sel = Math.round(Math.random());
-            data = data + sel;
-            f = Freq[sel];
-            t = p;
-        }
-        nowBuffering[i] = Math.sin(2 * Math.PI * f * t);
-        //  console.log(nowBuffering[i]);
-        t += p;
+  console.clear();
+  // Fill the buffer with 9k6;
+  //just random values between -1.0 and 1.0
+  // This gives us the actual array that contains the data
+  var nowBuffering = [];
+  var t = 0;
+  var p = 1 / (audioCtx.sampleRate * oversamplerate);
+  var sel = Math.round(Math.random());
+  var data = "Data:- " + sel;
+  var f = Freq[sel];
+  for (var i = 0; i < frameCount; i++) {
+    if (t >= (Loops / Freq[1])) {
+      sel = Math.round(Math.random());
+      data = data + sel;
+      f = Freq[sel];
+      t = p;
     }
-    console.log(data);
-    playbuffer(nowBuffering);
+    nowBuffering[i] = Math.sin(2 * Math.PI * f * t);
+    //  console.log(nowBuffering[i]);
+    t += p;
+  }
+  console.log(data);
+  playbuffer(nowBuffering);
 }
 
 function playbuffer(inputBuffer) {
-    myScope.run(inputBuffer);
-    var myArrayBuffer = audioCtx.createBuffer(channels, frameCount / oversamplerate, audioCtx.sampleRate);
-    var nowBuffering = myArrayBuffer.getChannelData(channel);
-    for (var channel = 0; channel < channels; channel++) {
-        for (var i = 0; i < (frameCount / oversamplerate); i += 1) {
-            nowBuffering[i] = inputBuffer[i * oversamplerate] * 0.01;
-        }
+  myScope.run(inputBuffer);
+  var myArrayBuffer = audioCtx.createBuffer(channels, frameCount / oversamplerate, audioCtx.sampleRate);
+  var nowBuffering = myArrayBuffer.getChannelData(channel);
+  for (var channel = 0; channel < channels; channel++) {
+    for (var i = 0; i < (frameCount / oversamplerate); i += 1) {
+      nowBuffering[i] = inputBuffer[i * oversamplerate] * 0.01;
     }
-    // Get an AudioBufferSourceNode.
-    // This is the AudioNode to use when we want to play an AudioBuffer
-    var source = audioCtx.createBufferSource();
-    // set the buffer in the AudioBufferSourceNode
-    source.buffer = myArrayBuffer;
-    // connect the AudioBufferSourceNode to the
-    // destination so we can hear the sound
-    source.connect(audioCtx.destination);
-    // start the source playing
-    source.start();
+  }
+  // Get an AudioBufferSourceNode.
+  // This is the AudioNode to use when we want to play an AudioBuffer
+  var source = audioCtx.createBufferSource();
+  // set the buffer in the AudioBufferSourceNode
+  source.buffer = myArrayBuffer;
+  // connect the AudioBufferSourceNode to the
+  // destination so we can hear the sound
+  source.connect(audioCtx.destination);
+  // start the source playing
+  source.start();
 }

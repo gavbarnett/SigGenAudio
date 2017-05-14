0875ae15c85114bda57e549263b3db9825338a5d
Gavin Barnett <gavbarnett@gmail.com>
2017-05-14 11:40:38 +0100

    Grid
    Adding a Grid to the Scope trace

--- assets/js/Scope.js
+++ assets/js/Scope.js
@@ -23,21 +23,44 @@ var myScope = {
     }
     this.clear();
     var ctx = this.context;
-    ctx.beginPath();
-    ctx.moveTo(0, canvassize.y / 2);
     var scope = [];
     var MaxX = Math.round(1000 * TimeBase);
+    //grid
+    ctx.strokeStyle = '#777777'; //dark-red
+    ctx.lineWidth = 2;
+    ctx.shadowBlur = 0;
+    //horizontal lines
+    h_lines = 6;
+    for (var i = 0; i < h_lines; i++) {
+      ctx.beginPath();
+      ctx.moveTo(0, canvassize.y / h_lines * i);
+      ctx.lineTo(canvassize.x, canvassize.y / h_lines * i);
+      ctx.stroke();
+      //ctx.arc(scope.x, scope.y, 1, 0, 2 * Math.PI);
+    }
+    //vertical lines
+    v_lines = 50;
+    for (var i = HorzTime; i < Math.min((MaxX + HorzTime), frameCount); i += TimeBase * v_lines) {
+      ctx.beginPath();
+      scope.x = (canvassize.x / MaxX * (i - HorzTime));
+      scope.y = (-1 * data[i] * canvassize.y / 2 * 1) + (canvassize.y / 2);
+      ctx.moveTo(scope.x, 0);
+      ctx.lineTo(scope.x, canvassize.y);
+      ctx.stroke();
+    }
+    ctx.beginPath();
+    ctx.moveTo(0, canvassize.y / 2);
     for (var i = HorzTime; i < Math.min((MaxX + HorzTime), frameCount); i += TimeBase) {
       scope.x = (canvassize.x / MaxX * (i - HorzTime));
       scope.y = (-1 * data[i] * canvassize.y / 2 * 1) + (canvassize.y / 2);
       ctx.lineTo(scope.x, scope.y);
       //ctx.arc(scope.x, scope.y, 1, 0, 2 * Math.PI);
     }
-    ctx.strokeStyle = '#20c20e';
-    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#20c20e'; //hacker green
+    ctx.lineWidth = 2;
     ctx.lineCap = 'round';
-    ctx.shadowColor = '#dddddd';
-    ctx.shadowBlur = 3;
+    //ctx.shadowColor = '#dddddd';
+    //  ctx.shadowBlur = 3;
     ctx.stroke();
   }
 }
